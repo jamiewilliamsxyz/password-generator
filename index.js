6193dba4cf39825b6dca04159c217801d6aeb88f
jamiewilliamsxyz <jamie7williams@gmail.com>
2025-08-25 14:53:05 +0100

    Display a test password when the button is clicked

---
+++ index.js
@@ -0,0 +1,17 @@
+let outputEl1 = document.getElementById("output-el1");
+let outputEl2 = document.getElementById("output-el2");
+
+let password1 = "";
+let password2 = "";
+
+const generatePassword = () => {
+  return "testPassword";
+};
+
+document.getElementById("generate-btn").addEventListener("click", () => {
+  password1 = generatePassword();
+  password2 = generatePassword();
+
+  outputEl1.textContent = password1;
+  outputEl2.textContent = password2;
+});
